ec5ca81b2812fae199d3c053a1129fd3f71d8cb7
Ppraveengouda <patilpraveen42@gmail.com>
2017-02-14 22:47:38 +0530

    Home activity

--- HangalKumaraswamiji/app/src/main/java/com/myrule/hangalkumaraswamiji/MainActivity.java
+++ HangalKumaraswamiji/app/src/main/java/com/myrule/hangalkumaraswamiji/MainActivity.java
@@ -1,21 +1,88 @@
 package com.myrule.hangalkumaraswamiji;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView listView;
+    private ListView listView;
+    private String names[] = {
+            "About",
+            "Download PDF",
+            "Login/Sign up",
+    };
+
+    private String desc[] = {
+            "Read about early life, Education ",
+            "Cascading Style Sheets",
+            "Code with Java Script",
+    };
+
+
+/*    private Integer imageid[] = {
+            R.drawable.html,
+            R.drawable.css,
+            R.drawable.js,
+            R.drawable.wp
+    };*/
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView=(ListView)findViewById(R.id.list_view);
-        String values[]=new String[]{
-                "About","Download PDF","Login/Sign up"
-        };
-        //asdsadsad
+        CustomList customList = new CustomList(this, names, desc);
+
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(customList);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),"You Clicked "+names[i],Toast.LENGTH_SHORT).show();
+            }
+        });
+        LinearLayout layout = (LinearLayout) findViewById(R.id.linear_scroll);
+        for (int i = 0; i < 10; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setId(i);
+            imageView.setPadding(2, 2, 2, 2);
+            imageView.setImageBitmap(BitmapFactory.decodeResource(
+                    getResources(), R.drawable.hanagalkumar01));
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            layout.addView(imageView);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
